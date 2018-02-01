cda0c758d264f32d53ef9aa23c090a4decf1f857
ilyesbb <ilyes.boussama@yahoo.fr>
2018-02-01 09:16:25 +0000

    rm private Vecteur

--- Arene/Vecteur.py
+++ Arene/Vecteur.py
@@ -15,17 +15,17 @@ class Vecteur(Point):
     def __mul__(self, vecteur):
         """ produit scalaire"""
         if issubclass(type(vecteur), Vecteur) and vecteur:
-            return self._x*vecteur._x+self._y*vecteur._y+self._z*vecteur._z
+            return self.x*vecteur.x+self.y*vecteur.y+self.z*vecteur.z
 
     def __pow__(self, vecteur):
         """produit vectoriel """
         if issubclass(type(vecteur), Vecteur) and vecteur:
-            return Vecteur(self._y*vecteur._z-self._z*vecteur._y, self._z*vecteur._x-self._x*vecteur._z, self._x*vecteur._y-self._y*vecteur._x)
+            return Vecteur(self.y*vecteur.z-self.z*vecteur.y, self.z*vecteur.x-self.x*vecteur.z, self.x*vecteur.y-self.y*vecteur.x)
 
     def __add__(self, vecteur):
         """ addition"""
         if issubclass(type(vecteur), Vecteur) and vecteur:
-            return Vecteur(self._x+vecteur._x, self._y+vecteur._y, self._z+vecteur._z)
+            return Vecteur(self.x+vecteur.x, self.y+vecteur.y, self.z+vecteur.z)
 
     def getAngle2D(self):
         """ Retourne l'angle du vecteur par rapport a la verticale, dans le sens trigo, entre pi et -pi"""
@@ -36,29 +36,29 @@ class Vecteur(Point):
         if issubclass(type(vecteur), Vecteur) and vecteur:
             # v: self^vecteur
             v=self**vecteur
-            if self._y != 0 and vecteur._x != 0:
+            if self.y != 0 and vecteur.x != 0:
                 #utilise les proprietes du produit vectoriel pour determiner si l'angle est positif ou negatif
-                if v._z>0:
+                if v.z>0:
                     return -acos((self*vecteur)/(self.getNorme()*vecteur.getNorme()))
-                elif v._z<0:
+                elif v.z<0:
                     return acos((self*vecteur)/(self.getNorme()*vecteur.getNorme()))
-            return atan2(vecteur._y, vecteur._x)-atan2(self._y, self._x)
+            return atan2(vecteur.y, vecteur.x)-atan2(self.y, self.x)
                 
     def getNorme(self):
         """Retourne la norme du vecteur """
-        return sqrt(pow(self._x,2)+pow(self._y,2)+pow(self._z,2))
+        return sqrt(pow(self.x,2)+pow(self.y,2)+pow(self.z,2))
     
     def rotation2D(self, teta):
         """tourne le vecteur d'un angle teta"""
         """pour tourner d'un angle teta, il vaut mieux que la norme du vecteur soit > a 2.pi/teta environ (pour une bonne precision sur les coordonnees)"""
         # n: norme
         # x: copie de self._x
-        n=sqrt(pow(self._x,2)+pow(self._y,2))
-        x=self._x
+        n=sqrt(pow(self.x,2)+pow(self.y,2))
+        x=self.x
         
-        self._x=(int)(x*cos(teta)-self._y*sin(teta))
-        self._y=(int)(x*sin(teta)+self._y*cos(teta))
-        n2=sqrt(pow(self._x,2)+pow(self._y,2))
+        self.x=(int)(x*cos(teta)-self.y*sin(teta))
+        self.y=(int)(x*sin(teta)+self.y*cos(teta))
+        n2=sqrt(pow(self.x,2)+pow(self.y,2))
         
         self.__errNorme=self.__errNorme+n-n2
         
@@ -66,6 +66,6 @@ class Vecteur(Point):
         if self.__errNorme>self.SEUIL_ERR:
             #ajustement de la norme
             a=self.getAngle2D()
-            self._x=(int)((n2+self.__errNorme)*cos(a))
-            self._y=(int)((n2+self.__errNorme)*sin(a))
-            self.__errNorme=self.__errNorme-(sqrt(pow(self._x,2)+pow(self._y,2))-n2)
+            self.x=(int)((n2+self.__errNorme)*cos(a))
+            self.y=(int)((n2+self.__errNorme)*sin(a))
+            self.__errNorme=self.__errNorme-(sqrt(pow(self.x,2)+pow(self.y,2))-n2)
