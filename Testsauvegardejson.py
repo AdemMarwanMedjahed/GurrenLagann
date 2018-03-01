b1e6c6a449e6dedd61442a82ce8d931e612ff063
lionkeita <35796183+lionkeita@users.noreply.github.com>
2018-03-01 11:37:26 +0100

    Add files via upload

---
+++ Testsauvegardejson.py
@@ -0,0 +1,24 @@
+from geometrie3D import *
+import filecmp
+
+
+#Creation d'une arene et ajout de n objets
+a = Arene()
+poly = Polygone3D()
+p = Pave(10, 10, 10)
+#p.tournerAutour(Point(0,4,5) , pi/3)
+poly.sommets = [Point(14, 15, 14), Point(5, 99, 120)]
+#a.add(poly)
+a.add(p)
+a.add(poly)
+a.sauvegarder("./geometrie3D/BanqueArenes/arene1.txt")
+b = Arene(list())
+a.sauvegardeArenejson("sauvegarde_a.txt")
+b = a.lireArenejson("sauvegarde_a.txt")
+b.sauvegardeArenejson("sauvegarde_b.txt")
+c = b.lireArenejson("sauvegarde_b.txt")
+c.sauvegardeArenejson("sauvegarde_c.txt")
+
+
+import filecmp
+print(filecmp.cmp('sauvegarde_a.txt', 'sauvegarde_c.txt'))
